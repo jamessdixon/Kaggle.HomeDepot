81f273dd6ea6c6bd90b0f209d0ac85705e790501
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2016-03-19 20:20:55 -0700

    removed uninformative features

--- ProductSearchRelevance.FSharp/ProductSearchRelevance/champion.fsx
+++ ProductSearchRelevance.FSharp/ProductSearchRelevance/champion.fsx
@@ -8,40 +8,39 @@ open HomeDepot.Features
 let features = 
     [| 
         ``Unique search terms matched in title``        
-        ``% unique search terms matched in title``          // 0.495331 
-        ``Unique search terms matched in description``      // 0.490977
-        ``% unique search terms matched in description``    // 0.490608
-        ``Unique search terms``                             // 0.489939
-        ``Duplicate search terms``                          // 0.489816
-        ``Search terms length``                             // 0.487736
-        ``Brand match in search terms``                     // 0.487680
-        ``First search terms and title words match``        // 0.487031
-        ``Last search terms and first title words match``   // 0.486734
-        ``Position of search terms in title``               // 0.487033
-        ``Reverse position of search terms in title``       // 0.485098
+        ``% unique search terms matched in title``           
+        ``Unique search terms matched in description``
+        ``% unique search terms matched in description``
+        ``Unique search terms``
+//        ``Duplicate search terms``                          
+        ``Search terms length``                             
+        ``Brand match in search terms``
+        ``First search terms and title words match``
+//        ``Last search terms and first title words match``
+        ``Position of search terms in title``
+        ``Reverse position of search terms in title``
 //        ``Unmatched search terms in title``
-        ``Longest matching seq between search terms and title``     // 0.484862
-        ``Longest backwards matching seq between search terms and title`` // 0.484120
-        ``Number of non-bullet attributes``                 // 0.478353
-//        ``Number of bullet attributes``                     // 0.484141 !?
-//        ``Number of attributes``                            // 0.478329
-        // re-added duplicate search terms                  // 0.478259
-        ``Search terms contain number``                     // 0.474616 after fixing attributes bug
-        ``Close product weight``                            // 0.474410
-        ``Has weight``                                      // 0.473804
-        ``Close product length``
-        ``Has length``                                      // 0.473695
-        ``Number of matching attribute names``              // 0.470940
-        ``Last search terms and title words match``         // 0.470988, !?
-        ``Search terms specificity``                        // 0.465452 > Kaggle 0.48527
-        ``Specificity weighted Search terms match``         // 0.463280
-        ``Frequency weighted title match``                  // 0.461863 > Kaggle 0.47902
-        ``Contains a surface``                              // 0.461150
-        ``Brand mismatch``                                  // 0.461061
-        ``Title contains last search term``                 // 0.455789 - with modified aggressive pre-proc
-        ``Product type match``                              // 0.455558
+        ``Longest matching seq between search terms and title``
+        ``Longest backwards matching seq between search terms and title``
+        ``Number of non-bullet attributes``
+//        ``Number of bullet attributes``
+//        ``Number of attributes``
+        ``Search terms contain number``
+//        ``Close product weight``
+        ``Has weight``
+//        ``Close product length``
+        ``Has length``
+        ``Number of matching attribute names``
+        ``Last search terms and title words match``
+        ``Search terms specificity``
+        ``Specificity weighted Search terms match``
+        ``Frequency weighted title match``
+//        ``Contains a surface``
+        ``Brand mismatch``
+        ``Title contains last search term``
+        ``Product type match``
         ``Bigrams title match``
-        ``Trigrams title match``
+//        ``Trigrams title match``
     |]
 
 let learner (sample:Example[]) =
@@ -88,6 +87,136 @@ let learner (sample:Example[]) =
 
 //evaluate 10 learner
 
-let test = learner trainset
+let model = learner trainset
 
 //createSubmission learner
+
+(*
+// worst mistakes
+
+trainset
+|> Array.map (fun (l,o) -> l, model o, o.SearchTerm, o.Product.Title)
+|> Seq.sortByDescending (fun (a,b,c,d) -> abs (a-b))
+|> Seq.take 100
+|> Seq.toArray
+
+*)
+
+(*
+// create file with extracted features
+
+let createFeaturesFile (sample:Example[]) =
+
+    let features = 
+        featurizer features (sample |> Array.map snd)
+
+    printfn "Preparing data"
+
+    let data = 
+        sample
+        |> Array.Parallel.map (fun (label,observation) ->
+            let fs = observation |> extract features
+            let row = Array.append [| label |] fs
+            row 
+            |> Array.map (sprintf "%.4f")
+            |> String.concat ",")
+
+    let desktop =
+        System.Environment.SpecialFolder.Desktop
+        |> System.Environment.GetFolderPath
+
+    let path = System.IO.Path.Combine(desktop,"features.csv")
+
+    System.IO.File.WriteAllLines(path,data)
+
+createFeaturesFile trainset
+*)
+
+(*
+// analysis of features importance
+    
+let featuresImportance () =
+
+    let desktop =
+        System.Environment.SpecialFolder.Desktop
+        |> System.Environment.GetFolderPath
+
+    let path = System.IO.Path.Combine(desktop,"features.csv")
+
+    let labels, data =
+        System.IO.File.ReadAllLines(path)
+        |> Array.map (fun line -> 
+            line.Split ',' 
+            |> Array.map float 
+            |> fun x -> x.[0], x.[1..])
+        |> Array.unzip
+
+    printfn "train"
+    let model = 
+
+        let trees = 600
+        let proportionHeldOut = 0.1
+        let sampleSize = data.Length
+        let featureCount = data.[0].Length
+        let featuresUsed = sqrt (float featureCount) |> ceil |> int
+
+        let trainData = 
+            Array.zip data labels
+            |> Array.map (fun (fs,ls) -> Array.append fs [| ls |])
+            |> array2D
+
+        let _info, forest, forestReport =
+            alglib.dfbuildrandomdecisionforestx1(
+                trainData, 
+                sampleSize, 
+                featureCount, 
+                1, 
+                trees, 
+                featuresUsed, 
+                proportionHeldOut)
+
+        let predictor (fs:float[]) = 
+            let mutable result : float [] = [||]
+            alglib.dfprocess(forest, fs, &result)
+            result.[0]
+
+        predictor
+
+    let data = data.[..9999]
+    let labels = labels.[..9999]
+    let size = data.[0].Length
+    let length = data.Length
+
+    let rng = System.Random(123456)
+
+    let perturb n i =
+        let copy = data |> Array.map(Array.copy)
+        for _ in 1 .. n do
+            let orig = rng.Next(length)
+            let dest = rng.Next(length)
+            copy.[dest].[i] <- data.[orig].[i]
+        copy
+
+    let rmse sample =
+        sample
+        |> Seq.averageBy (fun (act, exp) ->
+            let delta = act - exp
+            delta * delta)
+        |> sqrt
+
+    printfn "test features"
+
+    [ 
+        for f in 0 .. (size - 1) ->
+            printfn "  features %i" f
+
+            let data' = perturb 5000 f
+            let predictions = data' |> Array.map model
+            
+            let r = 
+                Array.zip labels predictions
+                |> rmse 
+            f, r
+    ]
+
+*)
